2f4cef0d13eb34a42ee98aeb704e7ea90f00202a
faa-rassoul <fatdiopgningue@gmail.com>
2025-06-14 11:40:46 +0100

    Update calculatrice.c

--- calculatrice.c
+++ calculatrice.c
@@ -3,31 +3,19 @@
 
 
 //fonction addition par Fatou 
+float addition(float a, float b) {
+    return a+b;
+}
 
-
-//fonction soustraction par saliou
 //fonction soustraction par saliou
 float soustraction(float a, float b) {
     return a - b;
 }
 
-//fonction division par Djiby
-float division(float a, float b) { 
-    return a / b;
-}
+//fonction division par Djiby 
 
 
 //fonction multiplication par Aida
-#include<stdio.h>
-
-float n1, n2, resultat,multiplication;
-printf("Entrez le premier n1: ");
-scanf("%f", &n1);
-printf("Entrez le deuxieme n2: ");
-scanf("%f", &n2);
-resultat = n1 * n2;
-printf("Le resultat de la multiplication est: %.2f\n", resultat);
-return 0;
 
 
 //méthode main par cynthia
