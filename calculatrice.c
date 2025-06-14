a9d742c2575f8fffaee6583d06c25ace2d3bff17
AMNWK <charonneuh@gmail.com>
2025-06-14 11:32:28 +0000

    Update calculatrice.c

--- calculatrice.c
+++ calculatrice.c
@@ -12,6 +12,17 @@
 
 
 //fonction multiplication par Aida
-
+#include<stdio.h>
+main() {
+    
+float n1, n2, resultat,multiplication;
+printf("Entrez le premier n1: ");
+scanf("%f", &n1);
+printf("Entrez le deuxieme n2: ");
+scanf("%f", &n2);
+resultat = n1 * n2;
+printf("Le resultat de la multiplication est: %.2f\n", resultat);
+return 0;
+}
 
 //méthode main par cynthia
