333fba9991e9513b368eba18035b0f24effc2bbc
Cyn8787 <cynthiadiadhiou@gmail.com>
2025-06-14 11:21:36 +0000

    Create calculatrice.c

---
+++ calculatrice.c
@@ -0,0 +1,17 @@
+
+//directive de compilation par Aminata
+
+
+//fonction addition par Fatou 
+
+
+//fonction soustraction par saliou
+
+
+//fonction division par Djiby 
+
+
+//fonction multiplication par Aida
+
+
+//méthode main par cynthia
